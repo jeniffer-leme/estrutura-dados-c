5ddf0e4d40bafc39f8412da0132db4ad6ee7d0bb
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-06-02 11:12:29 -0300

    Add intersection, difference, and merge functions

--- 05-Lista-Encadeada/include/listaEncadeada.h
+++ 05-Lista-Encadeada/include/listaEncadeada.h
@@ -4,12 +4,17 @@ struct elemento {
 
 typedef struct tipoNo *Lista; 
 
-
 Lista* criarLista();
 
 int inserirOrdenada(Lista* inicio, struct elemento valor);
 
 int removerElemento(Lista* inicio, int valor);
+
+int intersecao(Lista* inicio, struct elemento valor);
+
+int diferenca(Lista* inicio, struct elemento valor);
+
+int merge(Lista* inicio, struct elemento valor);
 
 void imprimirLista(Lista* inicio);
 
