69e78d6dfc12e620fc9116b7dd203c7a60c4ea26
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-06-01 22:50:33 -0300

    Create main.c

---
+++ 05-Lista-Encadeada/src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "listaEncadeada.h"
+
+
+int main() {
+    Lista *inicio = criarLista();
+
+    printf("Lista criada com sucesso!\n");
+
+
+    return 0;
+}
