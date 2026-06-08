d6bffbdad972823ee6a54ca0431d5bba7e540071
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-06-07 22:53:36 -0300

    Add header file for circular dynamic list structure

---
+++ 07-Lista-Dinamica-Circular/listaCircular.h
@@ -0,0 +1,22 @@
+struct elemento {
+    int matricula;
+    char nome;
+    float nota1, nota2;
+};
+
+typedef struct tipoNo *Lista;
+
+Lista* criarLista(Lista* inicio);
+
+int insereInicio(Lista* inicio, struct elemento aluno);
+int insereFinal(Lista* inicio, struct elemento aluno);
+int insereOrdenado(Lista* inicio, struct elemento aluno);
+
+int removeMatricula(Lista* inicio, int matricula);
+int removeInicio(Lista* inicio);
+int removeFinal(Lista* inicio);
+
+int tamanhoLista(Lista* inicio);
+int listaVazia(Lista* inicio);
+void imprimeLista(Lista* inicio);
+void apagaLista(Lista* inicio);
