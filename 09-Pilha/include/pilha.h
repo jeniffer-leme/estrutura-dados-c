789dadc973e93e9a70c423e4067ae500fed6df53
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-06-10 19:30:36 -0300

    Update pilha.h

--- 09-Pilha/include/pilha.h
+++ 09-Pilha/include/pilha.h
@@ -4,8 +4,15 @@ struct elemento {
     float nota1, nota2;
 };
 
-typedef struct tipoNo *Pilha;
+typedef struct tipoPilha Pilha;
 
-Pilha* criarLista();
-void liberarPilha(Pilha* topo);
+Pilha* criarPilha();
+void apagaPilha(Pilha* topo);
 
+int push(Pilha* topo, struct elemento aluno);
+int pop(Pilha* topo);
+int consultaTopo(Pilha* topo, struct elemento *aluno);
+
+int tamanhoPilha(Pilha* topo);
+int pilhaVazia(Pilha* topo);
+void imprimePilha(Pilha* topo);
