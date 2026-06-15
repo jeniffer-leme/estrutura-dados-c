1b050858e9ae8801399e8aa644a60bf412d905c3
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-06-15 10:16:25 -0300

    Add produto.h for product management structure

---
+++ Lista-Exercicios/05-Produto/produto.h
@@ -0,0 +1,23 @@
+struct elemento {
+    int codigo;
+    char nome[40];
+    float preco;
+    int quantidade;
+    int data;
+};
+
+typedef struct tipoLista Lista;
+
+Lista* criarLista();
+void apagaLista(Lista* lista);
+
+int cadastrarProduto(Lista* lista, struct elemento produto);
+int apagarProduto(Lista* lista, int codigo);
+int atualizarCadastro(Lista* lista, int codigo, struct elemento produto);
+
+
+int buscarMenorPreco(Lista* lista);
+int buscarEstoqueInferior(Lista* lista);
+
+int gerarRelatorioCres(Lista* lista);
+int gerarRelatorioDesc(Lista* lista);
