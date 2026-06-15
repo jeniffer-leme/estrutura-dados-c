aff9aaf0b11a63b67ce25987a846e4e849abaedd
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-06-15 10:16:02 -0300

    Create produto.c

---
+++ Lista-Exercicios/05-Produto/produto.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "produto.h"
+
+struct tipoNo {
+    struct elemento dado;
+    struct tipoNo* prox;
+};
+
+struct tipoLista {
+    struct tipoNo* inicio;
+    int qtd;
+};
+
+typedef struct tipoNo noLista;
+
+Lista* criarLista() {
+    Lista* list = (Lista*) calloc(1, sizeof(Lista));
+
+    if(list != NULL) {
+        list->inicio = NULL;
+        list->qtd = 0;
+    }
+
+    return list;
+}
+
+int cadastrarProduto(Lista* lista, struct elemento produto) {
+    if(lista == NULL) {
+        return 0;
+    }
+
+    noLista* no = (noLista*) calloc(1, sizeof(noLista));
+
+    if(no == NULL) {
+        return 0;
+    }
+
+    no->dado = produto;
+
+    no->prox = lista->inicio;
+    lista->inicio = no;
+    lista->qtd++;
+
+    return 1;
+}
+
+int apagarProduto(Lista* lista, int codigo) {
+    if(lista == NULL || lista->inicio == NULL) {
+        return 0;
+    }
+
+    noLista* atual = lista->inicio, *ant = NULL;
+
+    while(atual != NULL && atual->dado.codigo != codigo) {
+        ant = atual;
+        atual = atual->prox;
+    };
+
+    if(atual == NULL) {
+        return 0;
+    }
+
+    if(atual == lista->inicio) {
+        lista->inicio = lista->inicio->prox;
+    } else {
+        ant->prox = atual->prox;
+    }
+
+    free(atual);
+    lista->qtd--;
+
+    return 1;
+}
+
+int atualizarCadastro(Lista* lista, int codigo, struct elemento produto) {
+    if(lista == NULL || lista->inicio == NULL) {
+        return 0;
+    }
+
+    noLista* atual = lista->inicio, *ant = NULL;
+
+    while(atual != NULL && atual->dado.codigo != codigo) {
+        ant = atual;
+        atual = atual->prox;
+    }
+
+    if(atual == NULL) {
+        return 0;
+    }
+
+    
+}
