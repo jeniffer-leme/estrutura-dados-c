1443c9f0125e63d930130b1fa2f2ca7f39766160
Jeniffer Leme <162703146+jeniffer-leme@users.noreply.github.com>
2026-06-16 20:18:03 -0300

    Refactor shape structures and update related functions

--- Lista-Exercicios/09-Formas/include/formas.h
+++ Lista-Exercicios/09-Formas/include/formas.h
@@ -1,16 +1,41 @@
-struct elemento {
-    int id;
+struct retangulo {
     float base;
     float altura;
 };
 
+struct triangulo {
+    float base;
+    float altura;
+};
+
+struct circulo {
+    float raio;
+};
+
 typedef struct tipoLista Lista;
+
+typedef enum {
+    RETANGULO,
+    TRIANGULO,
+    CIRCULO
+} tipoFormas;
+
+struct elementos {
+    int id;
+    tipoFormas formas;
+
+    union {
+        struct retangulo ret;
+        struct triangulo tri;
+        struct circulo circ;
+    } figura;
+};
 
 Lista* criarLista();
 void apagarLista(Lista* lista);
 
-int inserirForma(Lista* lista, struct elemento forma);
+int inserirForma(Lista* lista, struct elementos forma);
 int removerForma(Lista* lista, int id);
 
-int calcularArea(Lista* lista, struct elemento* forma);
+float calcularArea(Lista* lista, struct elementos* forma);
 void imprimir(Lista* lista);
